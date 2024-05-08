68cd2de4ee90cb008af0ab63d95285fd25e7431d
Clair <clareajara@gmail.com>
2024-05-08 19:39:38 +0300

    practise

---
+++ djangoPractise/APP2/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from.import views
+
+
+urlpatterns = [
+    path('', views.app2, name="app2"),
+    path('firstapp/', include("members.urls"))
+]
---
+++ djangoPractise/djangoPractise/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include("members.urls")),
+    path('app2/', include("APP2.urls"))
+]
