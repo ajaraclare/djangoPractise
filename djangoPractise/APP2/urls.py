cb51fc23cf8d82b9087f47af91909cac6d6134dc
Clair <clareajara@gmail.com>
2024-05-29 08:02:57 +0300

    Modifications

--- djangoPractise/APP2/urls.py
+++ djangoPractise/APP2/urls.py
@@ -4,5 +4,6 @@ from.import views
 
 urlpatterns = [
     path('', views.app2, name="app2"),
-    path('firstapp/', include("members.urls"))
+    path('firstapp/', include("members.urls")),
+    path('input/', views.input, name="input")
 ]
--- djangoPractise/APP2/views.py
+++ djangoPractise/APP2/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from.models import Info
 
 # Create your views here.
 
 def app2(request):
     return render(request, 'app2.html')
+
+def input(request):
+    if request.method == "POST":
+        name = request.POST.get ('Name')
+        email = request.POST.get ('Email')
+        password = request.POST.get('password')
+
+        data=Info(name=name, email=email, password=password)
+        data.save()
+
+    return render(request, 'app2.html')
+
+
